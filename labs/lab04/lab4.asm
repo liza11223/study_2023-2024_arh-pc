eed43b55989d60c6f0c9454467e1ffa1537f93fe
liza11223 <lk3093398@gmail.com>
2024-10-26 13:58:13 +0300

    add fales for lab04

--- labs/lab04/lab4.asm
+++ labs/lab04/lab4.asm
@@ -1,8 +1,8 @@
 ; lab4.asm
 SECTION .data                       ; Начало секции данных
-    lab5:     DB 'Киселева Елизавета',10 
+    lab4:     DB 'Киселева Елизавета',10 
                                     
-    lab5Len:  EQU $-lab5          ; Длина строки lab5
+    lab4Len:  EQU $-lab4          ; Длина строки lab4
     
 SECTION .text        ; Начало секции кода 
     GLOBAL _start    
@@ -10,8 +10,8 @@ SECTION .text        ; Начало секции кода
 _start:              ; очка входа в программу
     mov eax,4        ; Системный вызов для записи (sys_write)
     mov ebx,1        ; Описатель файла '1' - стандартный вывод
-    mov ecx,lab5    ; Адрес строки lab5 в ecx
-    mov edx,lab5Len ; Размер строки lab5
+    mov ecx,lab4    ; Адрес строки lab4 в ecx
+    mov edx,lab4Len ; Размер строки lab4
     int 80h          ; Вызов ядра
     
     mov eax,1        ; Системный вызов для выхода (sys_exit)
